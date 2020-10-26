b0daef198c463fbf9c040a1d10ce5eb0061c9bf5
Matt <mattshu67@gmail.com>
2020-10-25 21:20:22 -0400

    Rename main module and uploaded setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bayscrape',
+    version='0.2',
+    packages=find_packages(),
+    url='github.com/mattshu/bayscrape',
+    license='MIT',
+    author='Matt Kelley',
+    author_email='mattshu32@gmail.com',
+    description='Safely browse legal torrents from the comfort of your command-line.'
+)
