59968f3b1028a495d9813221312e21fb9aa623a4
Matt Kelley <mattshu67@gmail.com>
2020-11-15 11:53:38 -0500

    Added command line argument to for faster searching.
    e.g. `python bayscrape.py royalty-free music -audio`

--- bayscrape.py
+++ bayscrape.py
@@ -4,6 +4,7 @@ Browse legal torrents from the safety and comfort of your command-line interface
 """
 from bsutils import get_input, output
 from results import Results
+from sys import argv
 
 _WELCOME_MESSAGE = "\n".join((
     "bayscrape torrent fetcher",
@@ -15,16 +16,22 @@ _WELCOME_MESSAGE = "\n".join((
 
 def main() -> None:
     """Begin bayscrape application."""
+    query = ""
+    if len(argv) > 1:
+        query = " ".join(argv[1:])
     while True:
         output(_WELCOME_MESSAGE)
-        query = get_input()
+        if query:  # in case loaded from command line arguments
+            output(query)
+        else:
+            query = get_input()
         torrent_results = Results(query)
         if torrent_results.user_quits:
             break
         if torrent_results.is_empty():
             output("No results found. Try your query again.\n\n")
             continue
-
+        query = ""
         torrent_results.open_interface()
 
 
